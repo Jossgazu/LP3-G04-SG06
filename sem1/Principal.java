df5de7fc6897079f77c4e0fa2d48dfd773bd9e8e
Jossgazu <josse08m@gmail.com>
2023-08-16 19:49:01 -0500

    1erArchivo

---
+++ sem1/Principal.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        int vida, rp;
+        boolean fl = false;
+        vida  = 0;
+        Scanner sc = new Scanner(System.in);
+        do {
+            vida ++;
+            System.out.println("Vida:"+ vida +"\t¿Cual es tu numero secretro?");
+            rp = sc.nextInt();
+            if (rp == 1234)
+                fl = true;
+        } while (vida < 3 && !fl );
+        if (fl == true)
+            System.out.println("Adivinaste!!!!");
+        else
+            System.out.println("ups, perdiste...");
+        
+    }
+
+}
